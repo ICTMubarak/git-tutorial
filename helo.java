42577771ce21e36ca5f2afb4a8d9907fab753fa9
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-02-01 08:47:20 +0600

    modfy

--- helo.java
+++ helo.java
@@ -1,5 +1,9 @@
 public class Helo{
     punlic static void main(Sting[]args){
         system.out.println("VS code is Awesome");
+    greeting("Meubarak Hossain");
     }
-}
+    punlic static void greeting(string name){
+        system.out.println("Welcome" + name);
+    }
+} 
